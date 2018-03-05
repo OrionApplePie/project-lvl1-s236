8a8dbe467235873b72743394540f37ab174e006c
Alexander Ten <orionapplepie@yandex.ru>
2018-03-05 22:39:54 +1000

    Removed redundand require()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  let readlineSync = require('readline-sync');
   let userName = readlineSync.question('May I have Your name? ');
   console.log('Hello, ' + userName + '!');
 };

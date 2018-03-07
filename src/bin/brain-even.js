29b30f937148ae855cfb8241eec73453756ad394
Alexander Ten <orionapplepie@yandex.ru>
2018-03-07 23:44:41 +1000

    Added directory for games

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 import conversation from '..';
-import isEvenGame from '../even';
+import isEvenGame from '../games/even';
 
 console.log('Welcome to Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no".\n');

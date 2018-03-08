1ced9fde35d0cb732b52c2661cd376c5d7228f37
Alexander Ten <orionapplepie@yandex.ru>
2018-03-08 12:59:02 +1000

    Improved project architecture

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
-import readlineSync from 'readline-sync';
-import conversation from '..';
+import { cons } from 'hexlet-pairs';
+import { commonFlow } from '..';
 
 const MAX_NUM = 100;
 const QUESTION_COUNT = 3;
@@ -7,22 +7,10 @@ const RULES_STR = 'Answer "yes" if number even otherwise answer "no".';
 
 const randInt = max => Math.floor(Math.random() * Math.floor(max));
 
-export default () => {
-  const userName = conversation(RULES_STR);
-
-  for (let i = 0; i < QUESTION_COUNT; i += 1) {
-    const num = randInt(MAX_NUM);
-    const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
-
-    console.log(`Question: ${num}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer.toLowerCase() === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+const evenGameQuestionGenerator = () => {
+  const num = randInt(MAX_NUM);
+  const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
+  return cons(num, correctAnswer);
 };
+
+export default () => commonFlow(RULES_STR, QUESTION_COUNT, evenGameQuestionGenerator);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
 
-export default (rules) => {
+const conversation = (rules) => {
   console.log('Welcome to Brain Games!');
   if (rules) {
     console.log(`${rules}\n`);
+  } else {
+    console.log();
   }
   const userName = readlineSync.question('May I have Your name? ');
   console.log(`Hello, ${userName}!\n`);
 
   return userName;
 };
+
+const commonFlow = (rules, questionCount, questionGenerator) => {
+  const userName = conversation(rules);
+
+  for (let i = 0; i < questionCount; i += 1) {
+    const questionPair = questionGenerator();
+    const question = car(questionPair);
+    const correctAnswer = cdr(questionPair);
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { conversation, commonFlow };
